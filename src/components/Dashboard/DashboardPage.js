5f692135fccc7e21b11e3479e524653da6a8e344
Saimul Hasnain <saimul@3pillarglobal.com>
2020-12-09 09:08:45 +0530

    update

--- src/components/Dashboard/DashboardPage.js
+++ src/components/Dashboard/DashboardPage.js
@@ -3,11 +3,9 @@ import { useHistory } from 'react-router-dom';
 
 function DashboardPage() {
     const history = useHistory();
-    // if()
     return (
         <Container>
             <CardHeader title="Welcome to dashboard" />
-            {/* <Link to='/' >Logout</Link> */}
             <Button color="secondary" variant="contained" onClick={() => history.push('/')}>Logout</Button>
         </Container>
     );
